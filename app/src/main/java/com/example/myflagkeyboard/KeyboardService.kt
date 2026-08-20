930a768739278c1327f4ed978b302ba812b2ab58
dozdiran44-a11y <dozdiran44@gmail.com>
2026-08-20 04:11:21 +0330

    Create KeyboardService.kt

---
+++ app/src/main/java/com/example/myflagkeyboard/KeyboardService.kt
@@ -0,0 +1,26 @@
+package com.example.myflagkeyboard
+
+import android.inputmethodservice.InputMethodService
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+
+class KeyboardService : InputMethodService() {
+
+    override fun onCreateInputView(): View {
+
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.HORIZONTAL
+
+        val flagButton = Button(this)
+        flagButton.text = "🏳️"
+
+        flagButton.setOnClickListener {
+            currentInputConnection.commitText("🏳️", 1)
+        }
+
+        layout.addView(flagButton)
+
+        return layout
+    }
+}
